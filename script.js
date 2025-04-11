6156b18568b87b133ff6e91c02d2a19e308bc6e9
Santhosh Kumar <cpbala4@gmail.com>
2025-04-11 15:56:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,224 @@
-const _0x30d1f1=_0x37e5;(function(_0x26ad83,_0x52d735){const _0x2a095f=_0x37e5,_0x10d5e7=_0x26ad83();while(!![]){try{const _0x486871=parseInt(_0x2a095f(0x1ea))/0x1*(-parseInt(_0x2a095f(0x1c9))/0x2)+parseInt(_0x2a095f(0x1d0))/0x3+parseInt(_0x2a095f(0x1d5))/0x4+parseInt(_0x2a095f(0x1eb))/0x5*(-parseInt(_0x2a095f(0x1c3))/0x6)+parseInt(_0x2a095f(0x1df))/0x7+parseInt(_0x2a095f(0x1db))/0x8*(parseInt(_0x2a095f(0x1c8))/0x9)+-parseInt(_0x2a095f(0x1e8))/0xa;if(_0x486871===_0x52d735)break;else _0x10d5e7['push'](_0x10d5e7['shift']());}catch(_0xd96c35){_0x10d5e7['push'](_0x10d5e7['shift']());}}}(_0x3adc,0x6852c));const option={'enabled':!![],'chatButtonSetting':{'backgroundColor':'#25D366','position':'right','marginBottom':0x14,'marginRight':0x14,'marginLeft':0x14,'ctaText':!![],'ctaIconWATI':!![]}},styles='\x0a\x20\x20\x20\x20@import\x20url(\x27https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400&display=swap\x27);\x0a\x0a\x20\x20\x20\x20#whatsapp-chat-widget\x20{\x0a\x20\x20\x20\x20\x20\x20display:\x20'+(option[_0x30d1f1(0x1d2)]?_0x30d1f1(0x1d4):_0x30d1f1(0x1cd))+_0x30d1f1(0x1c0)+option['chatButtonSetting'][_0x30d1f1(0x1d1)]+_0x30d1f1(0x1e9)+(option[_0x30d1f1(0x1de)]['position']==='left'?_0x30d1f1(0x1e3)+option[_0x30d1f1(0x1de)]['marginLeft']+'px':_0x30d1f1(0x1e4)+option[_0x30d1f1(0x1de)]['marginRight']+'px')+';\x0a\x20\x20\x20\x20\x20\x20border-radius:\x2032px;\x0a\x20\x20\x20\x20\x20\x20border:\x202px\x20solid\x20#363636;\x0a\x20\x20\x20\x20\x20\x20box-shadow:\x204px\x206px\x200px\x20'+option[_0x30d1f1(0x1de)]['backgroundColor']+_0x30d1f1(0x1dd)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?_0x30d1f1(0x1dc):_0x30d1f1(0x1bf))+_0x30d1f1(0x1e6)+option[_0x30d1f1(0x1de)][_0x30d1f1(0x1be)]+_0x30d1f1(0x1c1)+option[_0x30d1f1(0x1de)][_0x30d1f1(0x1d1)]+_0x30d1f1(0x1cf)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1d8)]?'1px':'0')+_0x30d1f1(0x1d6)+option[_0x30d1f1(0x1de)][_0x30d1f1(0x1be)]+_0x30d1f1(0x1c2)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?_0x30d1f1(0x1e3)+option[_0x30d1f1(0x1de)][_0x30d1f1(0x1ce)]+'px':_0x30d1f1(0x1e4)+option['chatButtonSetting'][_0x30d1f1(0x1d7)]+'px')+_0x30d1f1(0x1ca)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1c7)]?_0x30d1f1(0x1c5):_0x30d1f1(0x1cd))+_0x30d1f1(0x1c6)+option[_0x30d1f1(0x1de)]['marginBottom']+_0x30d1f1(0x1e9)+(option[_0x30d1f1(0x1de)]['position']==='left'?'left:\x20'+option[_0x30d1f1(0x1de)][_0x30d1f1(0x1ce)]+'px':_0x30d1f1(0x1e4)+option['chatButtonSetting'][_0x30d1f1(0x1d7)]+'px')+_0x30d1f1(0x1e0)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?'0':'1')+_0x30d1f1(0x1c4)+option['chatButtonSetting']['backgroundColor']+';\x0a\x20\x20\x20\x20\x20\x20border-radius:\x2024px;\x0a\x20\x20\x20\x20\x20\x20padding:\x208px\x2016px;\x0a\x20\x20\x20\x20\x20\x20font-weight:\x20500;\x0a\x20\x20\x20\x20\x20\x20font-size:\x2014px;\x0a\x20\x20\x20\x20\x20\x20line-height:\x20150%;\x0a\x20\x20\x20\x20\x20\x20color:\x20#202020;\x0a\x20\x20\x20\x20\x20\x20cursor:\x20pointer;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box::before\x20{\x0a\x20\x20\x20\x20\x20\x20content:\x20\x27\x27;\x0a\x20\x20\x20\x20\x20\x20position:\x20absolute;\x0a\x20\x20\x20\x20\x20\x20top:\x20100%;\x0a\x20\x20\x20\x20\x20\x20'+(option['chatButtonSetting'][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?_0x30d1f1(0x1dc):_0x30d1f1(0x1bf))+':\x2029px;\x0a\x20\x20\x20\x20\x20\x20width:\x200;\x0a\x20\x20\x20\x20\x20\x20height:\x200;\x0a\x20\x20\x20\x20\x20\x20border-width:\x200\x200px\x2030px\x2030px;\x0a\x20\x20\x20\x20\x20\x20border-color:\x20transparent\x20transparent\x20white\x20transparent;\x0a\x20\x20\x20\x20\x20\x20border-style:\x20solid;\x0a\x20\x20\x20\x20\x20\x20transform:\x20rotate('+(option['chatButtonSetting'][_0x30d1f1(0x1e2)]==='left'?_0x30d1f1(0x1da):'270')+_0x30d1f1(0x1e7)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?'left':_0x30d1f1(0x1bf))+_0x30d1f1(0x1cb)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?_0x30d1f1(0x1bd):_0x30d1f1(0x1d3))+'\x20'+option['chatButtonSetting'][_0x30d1f1(0x1be)]+_0x30d1f1(0x1e5)+(option[_0x30d1f1(0x1de)][_0x30d1f1(0x1e2)]===_0x30d1f1(0x1dc)?_0x30d1f1(0x1da):_0x30d1f1(0x1e1))+_0x30d1f1(0x1bb);document[_0x30d1f1(0x1bc)]('whatsapp-chat-style')[_0x30d1f1(0x1d9)]=styles;function _0x37e5(_0x3ca0bd,_0x45bb2e){const _0x3adcbc=_0x3adc();return _0x37e5=function(_0x37e5d9,_0x1bd33f){_0x37e5d9=_0x37e5d9-0x1bb;let _0x1b99a4=_0x3adcbc[_0x37e5d9];return _0x1b99a4;},_0x37e5(_0x3ca0bd,_0x45bb2e);}function _0x3adc(){const _0x375ec6=['left',';\x0a\x20\x20\x20\x20\x20\x20padding:\x2024px\x2024px\x2016px;\x0a\x20\x20\x20\x20\x20\x20min-height:\x20auto;\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20flex-direction:\x20column;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20\x20\x20gap:\x2016px;\x0a\x20\x20\x20\x20\x20\x20pointer-events:\x20none;\x0a\x20\x20\x20\x20\x20\x20opacity:\x200;\x0a\x20\x20\x20\x20\x20\x20scale:\x200;\x0a\x20\x20\x20\x20\x20\x20transform-origin:\x20','chatButtonSetting','224763XyimMu',';\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-bubble-close-button\x20{\x0a\x20\x20\x20\x20\x20\x20height:\x2020px;\x0a\x20\x20\x20\x20\x20\x20min-width:\x2020px;\x0a\x20\x20\x20\x20\x20\x20background:\x20#000000;\x0a\x20\x20\x20\x20\x20\x20border-radius:\x2024px;\x0a\x20\x20\x20\x20\x20\x20cursor:\x20pointer;\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20\x20\x20order:\x20','270','position','left:\x20','right:\x20',');\x0a\x20\x20\x20\x20\x20\x20transform:\x20rotate(','\x20bottom;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-visible\x20{\x0a\x20\x20\x20\x20\x20\x20pointer-events:\x20auto;\x0a\x20\x20\x20\x20\x20\x20opacity:\x201;\x0a\x20\x20\x20\x20\x20\x20scale:\x201;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-transition\x20{\x0a\x20\x20\x20\x20\x20\x20transition:\x20scale\x20150ms\x20ease-in,\x20opacity\x20250ms\x20ease-in;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-logo-container\x20{\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-brand\x20{\x0a\x20\x20\x20\x20\x20\x20width:\x2052px;\x0a\x20\x20\x20\x20\x20\x20height:\x2052px;\x0a\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x0a\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-content-chat-welcome\x20{\x0a\x20\x20\x20\x20\x20\x20font-family:\x20\x27Outfit\x27,\x20sans-serif\x20!important;\x0a\x20\x20\x20\x20\x20\x20font-size:\x2020px;\x0a\x20\x20\x20\x20\x20\x20line-height:\x20150%;\x0a\x20\x20\x20\x20\x20\x20text-align:\x20center;\x0a\x20\x20\x20\x20\x20\x20color:\x20#000000;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-content-send-btn\x20{\x0a\x20\x20\x20\x20\x20\x20background-color:\x20#1D1D1B\x20!important;\x0a\x20\x20\x20\x20\x20\x20box-shadow:\x204px\x204px\x200px\x20','deg);\x0a\x20\x20\x20\x20\x20\x20z-index:\x201;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box::after\x20{\x0a\x20\x20\x20\x20\x20\x20content:\x20\x27\x27;\x0a\x20\x20\x20\x20\x20\x20position:\x20absolute;\x0a\x20\x20\x20\x20\x20\x20top:\x20100%;\x0a\x20\x20\x20\x20\x20\x20','10774180FnhLPT','px\x20!important;\x0a\x20\x20\x20\x20\x20\x20','24027kTpSZx','65165dMkqEB','deg);\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20@media\x20only\x20screen\x20and\x20(max-width:\x20600px)\x20{\x0a\x20\x20\x20\x20\x20\x20.wa-chat-box\x20{\x0a\x20\x20\x20\x20\x20\x20\x20\x20box-sizing:\x20border-box;\x0a\x20\x20\x20\x20\x20\x20\x20\x20min-width:\x200%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20right:\x2020px\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20left:\x2020px\x20!important;\x0a\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20}\x0a\x20\x20','getElementById','-5px\x20-2px\x200px','backgroundColor','right',';\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box\x20{\x0a\x20\x20\x20\x20\x20\x20background-color:\x20white;\x0a\x20\x20\x20\x20\x20\x20z-index:\x2016000160\x20!important;\x0a\x20\x20\x20\x20\x20\x20margin-bottom:\x2092px;\x0a\x20\x20\x20\x20\x20\x20min-width:\x20320px;\x0a\x20\x20\x20\x20\x20\x20position:\x20fixed\x20!important;\x0a\x20\x20\x20\x20\x20\x20bottom:\x20',';\x0a\x20\x20\x20\x20\x20\x20border-radius:\x208px;\x0a\x20\x20\x20\x20\x20\x20text-decoration:\x20none;\x0a\x20\x20\x20\x20\x20\x20cursor:\x20pointer;\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20\x20\x20padding:\x2010px\x2016px;\x0a\x20\x20\x20\x20\x20\x20min-height:\x2042px;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-content-send-btn-text\x20{\x0a\x20\x20\x20\x20\x20\x20font-family:\x20\x27Outfit\x27,\x20sans-serif\x20!important;\x0a\x20\x20\x20\x20\x20\x20font-weight:\x20500;\x0a\x20\x20\x20\x20\x20\x20font-size:\x2015px;\x0a\x20\x20\x20\x20\x20\x20line-height:\x2020px;\x0a\x20\x20\x20\x20\x20\x20color:\x20#FFFFFF\x20!important;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-poweredby\x20{\x0a\x20\x20\x20\x20\x20\x20margin-left:\x20auto;\x0a\x20\x20\x20\x20\x20\x20margin-right:\x20auto;\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20\x20\x20gap:\x203px;\x0a\x20\x20\x20\x20\x20\x20font-family:\x20\x27Outfit\x27,\x20sans-serif\x20!important;\x0a\x20\x20\x20\x20\x20\x20font-size:\x2012px;\x0a\x20\x20\x20\x20\x20\x20line-height:\x2018px;\x0a\x20\x20\x20\x20\x20\x20color:\x20#999999;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-box-poweredby-link\x20{\x0a\x20\x20\x20\x20\x20\x20font-weight:\x20600;\x0a\x20\x20\x20\x20\x20\x20color:\x20#666666\x20!important;\x0a\x20\x20\x20\x20\x20\x20text-decoration:\x20none\x20!important;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-widget-send-button\x20{\x0a\x20\x20\x20\x20\x20\x20margin:\x200\x200\x20',';\x0a\x20\x20\x20\x20\x20\x20box-shadow:\x204px\x205px\x2010px\x20rgba(0,\x200,\x200,\x200.4);\x0a\x20\x20\x20\x20\x20\x20','54PygDIQ',';\x0a\x20\x20\x20\x20\x20\x20color:\x20#fff;\x0a\x20\x20\x20\x20\x20\x20font-size:\x2014px;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-bubble-text\x20{\x0a\x20\x20\x20\x20\x20\x20font-family:\x20\x27Outfit\x27,\x20sans-serif\x20!important;\x0a\x20\x20\x20\x20\x20\x20background:\x20#FFFFFF;\x0a\x20\x20\x20\x20\x20\x20border:\x201px\x20solid\x20#363636;\x0a\x20\x20\x20\x20\x20\x20box-shadow:\x202px\x203px\x200px\x20','flex',';\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20\x20\x20gap:\x208px;\x0a\x20\x20\x20\x20\x20\x20z-index:\x2016000160\x20!important;\x0a\x20\x20\x20\x20\x20\x20position:\x20fixed\x20!important;\x0a\x20\x20\x20\x20\x20\x20margin-bottom:\x2063px;\x0a\x20\x20\x20\x20\x20\x20bottom:\x20','ctaText','31095GIYWpf','24ekCjUo',';\x0a\x20\x20\x20\x20\x20\x20cursor:\x20pointer;\x0a\x20\x20\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20\x20\x20\x20\x20align-items:\x20center;\x0a\x20\x20\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20.wa-chat-bubble\x20{\x0a\x20\x20\x20\x20\x20\x20display:\x20',':\x2027px;\x0a\x20\x20\x20\x20\x20\x20width:\x200;\x0a\x20\x20\x20\x20\x20\x20height:\x200;\x0a\x20\x20\x20\x20\x20\x20border-width:\x200px\x200px\x2034px\x2034px;\x0a\x20\x20\x20\x20\x20\x20border-color:\x20transparent\x20transparent\x20black\x20transparent;\x0a\x20\x20\x20\x20\x20\x20border-style:\x20solid;\x0a\x20\x20\x20\x20\x20\x20border-radius:\x202px;\x0a\x20\x20\x20\x20\x20\x20filter:\x20drop-shadow(','wa-chat-box-visible','none','marginLeft','px\x200\x20!important;\x0a\x20\x20\x20\x20\x20\x20position:\x20fixed\x20!important;\x0a\x20\x20\x20\x20\x20\x20z-index:\x2016000160\x20!important;\x0a\x20\x20\x20\x20\x20\x20bottom:\x200\x20!important;\x0a\x20\x20\x20\x20\x20\x20height:\x2052px;\x0a\x20\x20\x20\x20\x20\x20min-width:\x2052px;\x0a\x20\x20\x20\x20\x20\x20border:\x20','1082430afxujE','marginBottom','enabled','-2px\x205px\x200px','block','2849652jSwnGJ','\x20solid\x20#363636;\x0a\x20\x20\x20\x20\x20\x20border-radius:\x20100px;\x0a\x20\x20\x20\x20\x20\x20background-color:\x20','marginRight','ctaIconWATI','innerHTML','180','1864fYrbte'];_0x3adc=function(){return _0x375ec6;};return _0x3adc();}function toggleChatBox(){const _0x5c1341=_0x30d1f1,_0x297fbd=document[_0x5c1341(0x1bc)]('wa-chat-box');_0x297fbd['classList']['toggle'](_0x5c1341(0x1cc));}
+
+  const option = {
+    enabled: true,
+    chatButtonSetting: {
+      backgroundColor: '#25D366',
+      position: 'right',
+      marginBottom: 20,
+      marginRight: 20,
+      marginLeft: 20,
+      ctaText: true,
+      ctaIconWATI: true
+    }
+  };
+
+  const styles = `
+    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400&display=swap');
+
+    #whatsapp-chat-widget {
+      display: ${option.enabled ? 'block' : 'none'};
+    }
+
+    .wa-chat-box {
+      background-color: white;
+      z-index: 16000160 !important;
+      margin-bottom: 92px;
+      min-width: 320px;
+      position: fixed !important;
+      bottom: ${option.chatButtonSetting.marginBottom}px !important;
+      ${option.chatButtonSetting.position === 'left'
+        ? `left: ${option.chatButtonSetting.marginLeft}px`
+        : `right: ${option.chatButtonSetting.marginRight}px`};
+      border-radius: 32px;
+      border: 2px solid #363636;
+      box-shadow: 4px 6px 0px ${option.chatButtonSetting.backgroundColor};
+      padding: 24px 24px 16px;
+      min-height: auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 16px;
+      pointer-events: none;
+      opacity: 0;
+      scale: 0;
+      transform-origin: ${option.chatButtonSetting.position === 'left' ? 'left' : 'right'} bottom;
+    }
+
+    .wa-chat-box-visible {
+      pointer-events: auto;
+      opacity: 1;
+      scale: 1;
+    }
+
+    .wa-chat-box-transition {
+      transition: scale 150ms ease-in, opacity 250ms ease-in;
+    }
+
+    .wa-chat-logo-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .wa-chat-box-brand {
+      width: 52px;
+      height: 52px;
+     
+      
+
+    }
+
+    .wa-chat-box-content-chat-welcome {
+      font-family: 'Outfit', sans-serif !important;
+      font-size: 20px;
+      line-height: 150%;
+      text-align: center;
+      color: #000000;
+    }
+
+    .wa-chat-box-content-send-btn {
+      background-color: #1D1D1B !important;
+      box-shadow: 4px 4px 0px ${option.chatButtonSetting.backgroundColor};
+      border-radius: 8px;
+      text-decoration: none;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 10px 16px;
+      min-height: 42px;
+    }
+
+    .wa-chat-box-content-send-btn-text {
+      font-family: 'Outfit', sans-serif !important;
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 20px;
+      color: #FFFFFF !important;
+    }
+
+    .wa-chat-box-poweredby {
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 3px;
+      font-family: 'Outfit', sans-serif !important;
+      font-size: 12px;
+      line-height: 18px;
+      color: #999999;
+    }
+
+    .wa-chat-box-poweredby-link {
+      font-weight: 600;
+      color: #666666 !important;
+      text-decoration: none !important;
+    }
+
+    .wa-widget-send-button {
+      margin: 0 0 ${option.chatButtonSetting.marginBottom}px 0 !important;
+      position: fixed !important;
+      z-index: 16000160 !important;
+      bottom: 0 !important;
+      height: 52px;
+      min-width: 52px;
+      border: ${option.chatButtonSetting.ctaIconWATI ? '1px' : '0'} solid #363636;
+      border-radius: 100px;
+      background-color: ${option.chatButtonSetting.backgroundColor};
+      box-shadow: 4px 5px 10px rgba(0, 0, 0, 0.4);
+      ${option.chatButtonSetting.position === 'left'
+        ? `left: ${option.chatButtonSetting.marginLeft}px`
+        : `right: ${option.chatButtonSetting.marginRight}px`};
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .wa-chat-bubble {
+      display: ${option.chatButtonSetting.ctaText ? 'flex' : 'none'};
+      align-items: center;
+      gap: 8px;
+      z-index: 16000160 !important;
+      position: fixed !important;
+      margin-bottom: 63px;
+      bottom: ${option.chatButtonSetting.marginBottom}px !important;
+      ${option.chatButtonSetting.position === 'left'
+        ? `left: ${option.chatButtonSetting.marginLeft}px`
+        : `right: ${option.chatButtonSetting.marginRight}px`};
+    }
+
+    .wa-chat-bubble-close-button {
+      height: 20px;
+      min-width: 20px;
+      background: #000000;
+      border-radius: 24px;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      order: ${option.chatButtonSetting.position === 'left' ? '0' : '1'};
+      color: #fff;
+      font-size: 14px;
+    }
+
+    .wa-chat-bubble-text {
+      font-family: 'Outfit', sans-serif !important;
+      background: #FFFFFF;
+      border: 1px solid #363636;
+      box-shadow: 2px 3px 0px ${option.chatButtonSetting.backgroundColor};
+      border-radius: 24px;
+      padding: 8px 16px;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 150%;
+      color: #202020;
+      cursor: pointer;
+    }
+
+    .wa-chat-box::before {
+      content: '';
+      position: absolute;
+      top: 100%;
+      ${option.chatButtonSetting.position === 'left' ? 'left' : 'right'}: 29px;
+      width: 0;
+      height: 0;
+      border-width: 0 0px 30px 30px;
+      border-color: transparent transparent white transparent;
+      border-style: solid;
+      transform: rotate(${option.chatButtonSetting.position === 'left' ? '180' : '270'}deg);
+      z-index: 1;
+    }
+
+    .wa-chat-box::after {
+      content: '';
+      position: absolute;
+      top: 100%;
+      ${option.chatButtonSetting.position === 'left' ? 'left' : 'right'}: 27px;
+      width: 0;
+      height: 0;
+      border-width: 0px 0px 34px 34px;
+      border-color: transparent transparent black transparent;
+      border-style: solid;
+      border-radius: 2px;
+      filter: drop-shadow(${option.chatButtonSetting.position === 'left' ? '-5px -2px 0px' : '-2px 5px 0px'} ${option.chatButtonSetting.backgroundColor});
+      transform: rotate(${option.chatButtonSetting.position === 'left' ? '180' : '270'}deg);
+    }
+
+    @media only screen and (max-width: 600px) {
+      .wa-chat-box {
+        box-sizing: border-box;
+        min-width: 0%;
+        right: 20px !important;
+        left: 20px !important;
+      }
+    }
+  `;
+
+  document.getElementById("whatsapp-chat-style").innerHTML = styles;
+
+  function toggleChatBox() {
+    const box = document.getElementById("wa-chat-box");
+    box.classList.toggle("wa-chat-box-visible");
+  }
